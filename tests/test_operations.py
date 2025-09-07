c592e1fac985d5e27ac16df3c8ec01ef4a1f9cf0
bibhusahu91 <bibhu.sahu@quartech.com>
2025-09-06 23:24:49 -0400

    final chagesss

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,7 +1,7 @@
-from src.maths import app
+from src.app import app
 
 def test_home():
-    reponse = app.test_client.get("/")
-
-    assert reponse.status_code == 200
-    assert response.data == b"Hello World!"
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.data == b"Hello World!"   # exact match ✅
